469bb16fab0e2108c48a81a766d09261879a17ad
Deepak <20bcs102@nith.ac.in>
2023-09-05 23:34:45 +0530

    app-updated

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 connectDB();
 
 app.use("/api/users", userRoutes);
