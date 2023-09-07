eb733972a6d8be6a677a5c73dc09634351001be2
Deepak <20bcs102@nith.ac.in>
2023-09-07 18:25:21 +0530

    get all post route added

--- controllers/postController.js
+++ controllers/postController.js
@@ -37,6 +37,15 @@ const getPosts = async (req, res) => {
   }
 };
 
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate("author", "-password");
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 const updatePost = async (req, res) => {
   const { title, content } = req.body;
   const { id } = req.params;
@@ -71,4 +80,5 @@ module.exports = {
   getPosts,
   updatePost,
   deletePost,
+  getAllPosts,
 };
--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -5,9 +5,11 @@ const {
   getPosts,
   updatePost,
   deletePost,
+  getAllPosts,
 } = require("../controllers/postController");
 const authMiddleware = require("../middlewares/authMiddleware");
 router.post("/create-post", authMiddleware, createPost);
+router.get("/get-all-posts", getAllPosts);
 router.get("/get-post", authMiddleware, getPosts);
 router.put("/update-post/:id", authMiddleware, updatePost);
 router.delete("/delete-post/:id", authMiddleware, deletePost);
